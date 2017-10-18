3b970fb98ba156e1efe154e61ebc210621ddd27d
Amanda Pouget <amanda.pouget@rolemodelsoftware.com>
2017-10-18 15:55:00 -0400

    Make sure everything is working with combined scenarios.

--- lib/marsh_grass/rspec.rb
+++ lib/marsh_grass/rspec.rb
@@ -5,28 +5,6 @@ require 'timecop'
 require 'pry'
 
 RSpec.configure do |config|
-
-  config.around(repetitions: true) do |original_example|
-    # Fetch the number of repetitions to try...
-    repetitions = original_example.metadata[:repetitions]
-    total = repetitions.is_a?(Integer) ? repetitions : 1000
-    # Add repetition count to description
-    def modify_description(test, count, total)
-      test.metadata[:description] = "Repetition #{count} of #{total}: #{test.metadata[:description]}"
-    end
-
-    (1..total).each do |count|
-      # Let the original example be the final repetition
-      example = if count < total
-        original_example.duplicate_with(repetitions: false) # ensure tag does not trigger
-      else
-        original_example
-      end
-      modify_description(example, count, total)
-      example.run(original_example.example_group_instance, original_example.reporter)
-    end
-  end
-
   config.around(time_of_day: true) do |original_example|
     now = Time.now
     time_of_day = original_example.metadata[:time_of_day]
@@ -38,8 +16,7 @@ RSpec.configure do |config|
 
     total = hours_to_run.size * minutes_to_run.size * seconds_to_run.size
 
-    # Add time of day to our test description
-    def modify_description(test, hour, minute, second)
+    def describe_time_of_day(test, hour, minute, second)
       test.metadata[:description] = "Run Time #{hour}:#{minute}:#{second}: #{test.metadata[:description]}"
     end
 
@@ -56,9 +33,10 @@ RSpec.configure do |config|
             example = if run_count < total
               original_example.duplicate_with(time_of_day: false) # ensure tag does not trigger
             else
+              original_example.metadata[:time_of_day] = false
               original_example
             end
-            modify_description(example, hour, minute, second)
+            describe_time_of_day(example, hour, minute, second)
             example.run(original_example.example_group_instance, original_example.reporter)
           end
         end
@@ -75,8 +53,7 @@ RSpec.configure do |config|
     # 1000 milliseconds before & after the surrounding time
     test_time_float = Time.local(now.year, now.month, now.day, hour, minute, second).to_f
 
-    # Add exact time to our test description
-    def modify_description(test, time)
+    def describe_exact_time(test, time)
       test.metadata[:description] = "Run Time #{time.strftime('%H:%M:%S:%L')}: #{test.metadata[:description]}"
     end
 
@@ -91,17 +68,17 @@ RSpec.configure do |config|
         example = if millisecond < 1000
           original_example.duplicate_with(surrounding_time: false) # ensure tag doesn't trigger
         else
+          original_example.metadata[:surrounding_time] = false
           original_example
         end
-        modify_description(example, test_time)
+        describe_exact_time(example, test_time)
         example.run(original_example.example_group_instance, original_example.reporter)
       end
     end
   end
 
   config.around(elapsed_time: true) do |original_example|
-    # Add to test description the time elapsed while the test ran
-    def modify_description(test, scale)
+    def describe_time_elapsed(test, scale)
       test.metadata[:description] = "Run Speed #{scale}x Slower: #{test.metadata[:description]}"
     end
 
@@ -115,9 +92,10 @@ RSpec.configure do |config|
         example = if (seconds_multiplier != time_multipliers.last)
           original_example.duplicate_with(elapsed_time: false) # ensure tag doesn't trigger
         else
+          original_example.metadata[:elapsed_time] = false
           original_example
         end
-        modify_description(example, seconds_multiplier)
+        describe_time_elapsed(example, seconds_multiplier)
         example.run(original_example.example_group_instance, original_example.reporter)
       end
     end
@@ -128,8 +106,7 @@ RSpec.configure do |config|
     time_zone_hours = %w[-12 -11 -10 -09 -08 -07 -06 -05 -04 -03 -02 -01 +00 +01 +02 +03 +04 +05 +06 +07 +08 +09 +10 +11 +12 +13 +14]
     time_zone_minutes = %w[00 30]
 
-    # Add time zone to test description
-    def modify_description(test, time_zone_hour, time_zone_minute)
+    def describe_time_zone(test, time_zone_hour, time_zone_minute)
       test.metadata[:description] = "Time Zone Offset #{time_zone_hour}:#{time_zone_minute}: #{test.metadata[:description]}"
     end
 
@@ -146,12 +123,35 @@ RSpec.configure do |config|
           example = if run_count < total
             original_example.duplicate_with(time_zones: false)
           else
+            original_example.metadata[:time_zones] = false
             original_example
           end
-          modify_description(example, time_zone_hour, time_zone_minute)
+          describe_time_zone(example, time_zone_hour, time_zone_minute)
           example.run(original_example.example_group_instance, original_example.reporter)
         end
       end
+    end
+  end
+
+  config.around(repetitions: true) do |original_example|
+    # Fetch the number of repetitions to try...
+    repetitions = original_example.metadata[:repetitions]
+    total = repetitions.is_a?(Integer) ? repetitions : 1000
+
+    def describe_repetition_count(test, count, total)
+      test.metadata[:description] = "Repetition #{count} of #{total}: #{test.metadata[:description]}"
+    end
+
+    (1..total).each do |count|
+      # Let the original example be the final repetition
+      example = if count < total
+        original_example.duplicate_with(repetitions: false) # ensure tag does not trigger
+      else
+        original_example.metadata[:repetitions] = false
+        original_example
+      end
+      describe_repetition_count(example, count, total)
+      example.run(original_example.example_group_instance, original_example.reporter)
     end
   end
 end
